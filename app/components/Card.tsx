0bd768a62b5ee5601e01c7f40c83db9735af5263
erdincozturk <e.erdincoztrk@gmail.com>
2024-06-01 23:53:20 +0300

    - Styller uyumlu şekilde yeniden tanımlandı. - Pressable bileşeni TouchableOpacity bileşinene çevrildi.

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,57 +1,56 @@
-import React, { FC } from "react";
+import React, {FC, useState} from "react";
 import {
-  Button,
-  Image,
-  ImageStyle, Pressable, StyleSheet,
-  Text, TextStyle,
-  TouchableOpacity,
-  View,
-  ViewStyle,
+    Button,
+    Image,
+    ImageStyle, Pressable, StyleSheet,
+    Text, TextStyle,
+    TouchableOpacity,
+    View,
+    ViewStyle,
 } from "react-native";
-import { Pokemon } from "../models/pokemon";
+import {Pokemon} from "../models/pokemon";
 
 interface CardProps {
-  pokemon: Pokemon;
-  onPress: () => void;
+    pokemon: Pokemon;
+    onPress: () => void;
 }
 
-const Card: FC<CardProps> = ({ pokemon, onPress }) => {
-  return (
+const Card: FC<CardProps> = ({pokemon, onPress}) => {
 
-      <View style={$containerStyle}>
-        <Image
-          source={{ uri: pokemon.images.small }}
-          style={$imageStyle}
-        ></Image>
-        <Pressable
-            onPress={onPress}
-            style={styles.button}
+    return (
 
-        >
-          <Text style={styles.text}>See Details</Text>
-        </Pressable>
-      </View>
+        <View style={$containerStyle}>
+            <Image
+                source={{uri: pokemon.images.small}}
+                style={$imageStyle}
+            ></Image>
+            <TouchableOpacity
+                onPress={onPress}
+                style={$buttonStyle}
+            >
+                <Text style={$buttonTextStyle}>See Details</Text>
+            </TouchableOpacity>
+        </View>
 
-  );
+    );
 };
 
 const $containerStyle: ViewStyle = {
-  flex:4,
-  borderBottomWidth: 1,
-  borderBottomColor: "lightgray",
-  padding: 8,
-  marginBottom: 16,
-  alignContent: "center",
-  alignItems: "center",
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+    padding: 8,
+    marginBottom: 16,
+    alignContent: "center",
+    alignItems: "center",
 };
 
 const $imageStyle: ImageStyle = {
-  width: 100,
-  aspectRatio: 0.71,
+    width: 100,
+    aspectRatio: 0.71,
 };
 
-const styles = StyleSheet.create({
-  button: {
+const $buttonStyle: ViewStyle = {
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#add8e6",
@@ -60,12 +59,12 @@ const styles = StyleSheet.create({
     elevation: 4,
     width: 90,
     height: 30
-  },
-  text: {
-  fontWeight:"bold"
-  }
-})
+}
 
-const $rightSection: ViewStyle = { paddingLeft: 16 };
+const $buttonTextStyle: TextStyle = {
+    fontWeight: "bold"
+}
+
+const $rightSection: ViewStyle = {paddingLeft: 16};
 
 export default Card;
