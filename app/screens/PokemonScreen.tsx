d5e240de78430713c2b7bc43fb26f75f84f75ea2
hasankocal <57703964+hasankocal@users.noreply.github.com>
2024-06-07 16:59:35 +0300

    pokemonscreen refactor

--- app/screens/PokemonScreen.tsx
+++ app/screens/PokemonScreen.tsx
@@ -1,17 +1,26 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Text,
   View,
-  StyleSheet,
   ScrollView,
   Image,
+  ViewStyle,
+  TouchableOpacity,
+  Alert,
+  TextStyle,
+  ImageStyle,
 } from "react-native";
 import axios from "axios";
 import { AppStackScreenProps } from "../navigators/AppNavigator";
-import { Pokemon } from "../models/pokemon";
+import { Pokemon, PokemonDetail } from "../models/pokemon";
 
 interface PokemonScreenProps extends AppStackScreenProps<"Pokemon"> {}
+
+interface DetailContainerProps {
+  title: string;
+  details?: PokemonDetail[];
+}
 
 const PokemonScreen: FC<PokemonScreenProps> = (props) => {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
@@ -24,117 +33,94 @@ const PokemonScreen: FC<PokemonScreenProps> = (props) => {
       .get(`https://api.pokemontcg.io/v2/cards/${pokemonId}`)
       .then((response) => {
         setPokemon(response.data.data);
-        console.log(JSON.stringify(response.data.data));
+        //console.log(JSON.stringify(response.data.data));
       })
       .finally(() => setLoading(false));
   }, []);
 
-  return (
-    <View style={styles.container}>
-      {loading == true ? (
-        <ActivityIndicator size={"large"} />
-      ) : (
-        <ScrollView>
-          <Image source={{ uri: pokemon.images.large }} style={styles.image} />
-          <Text style={styles.name}>{pokemon.name}</Text>
-          <Text style={styles.type}>Type: {pokemon.types.join(", ")}</Text>
-          <Text style={styles.hp}>HP: {pokemon.hp}</Text>
-          {pokemon.abilities ? (
-            <Text style={styles.subTitle}>
-              Abilities:{" "}
-              {pokemon.abilities &&
-                pokemon.abilities.map((ability, index) => (
-                  <Text key={index} style={styles.ability}>
-                    {ability.name}: {ability.text}
-                  </Text>
-                ))}
-            </Text>
-          ) : (
-            <View>
-              <Text style={styles.subTitle}>Abilities: </Text>
-              <Text style={styles.ability}>No Abilities</Text>
+  const DetailContainer: FC<DetailContainerProps> = useCallback(
+    ({ title, details }) => {
+      return (
+        <View style={$detailContainerStyle}>
+          <Text style={$labelStyle}>{title}</Text>
+          {details ? (
+            <View style={$buttonContainerStyle}>
+              {details.map((detail) => (
+                <TouchableOpacity
+                  style={$buttonStyle}
+                  onPress={() => Alert.alert(detail.name, detail.text)}
+                >
+                  <Text>{detail.name}</Text>
+                </TouchableOpacity>
+              ))}
             </View>
+          ) : (
+            <Text>No detail</Text>
           )}
-          <Text style={styles.subTitle}>Attacks:</Text>
-          {pokemon.attacks &&
-            pokemon.attacks.map((attack, index) => (
-              <Text key={index} style={styles.attack}>
-                {attack.name} ({attack.cost.join(", ")}): {attack.damage} -{" "}
-                {attack.text}
-              </Text>
-            ))}
-          <Text style={styles.subTitle}>Weaknesses:</Text>
-          {pokemon.weaknesses &&
-            pokemon.weaknesses.map((weakness, index) => (
-              <Text key={index} style={styles.weakness}>
-                {weakness.type}: {weakness.value}
-              </Text>
-            ))}
-          <Text style={styles.subTitle}>Set Information:</Text>
-          <Text style={styles.set}>Set: {pokemon.set.name}</Text>
-          <Text style={styles.set}>Series: {pokemon.set.series}</Text>
-          <Text style={styles.set}>
-            Release Date: {pokemon.set.releaseDate}
-          </Text>
-          <Text style={styles.set}>Rarity: {pokemon.rarity}</Text>
-          <Text style={styles.set}>Artist: {pokemon.artist}</Text>
-        </ScrollView>
+        </View>
+      );
+    },
+    []
+  );
+
+  return (
+    <ScrollView contentContainerStyle={$contentContainerStyle}>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Image source={{ uri: pokemon.images.large }} style={$imageStyle} />
+
+          <View style={$labelContainer}>
+            <Text style={$labelStyle}>{pokemon.name}</Text>
+            <Text style={$labelStyle}>Type: {pokemon.types.join(", ")}</Text>
+            <Text style={$labelStyle}>HP: {pokemon.hp}</Text>
+          </View>
+
+          <DetailContainer title="Abilities" details={pokemon.abilities} />
+          <DetailContainer title="Attacks" details={pokemon.attacks} />
+        </>
       )}
-    </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 22,
-    backgroundColor: "#f8f8f8",
-  },
-  image: {
-    width: 300,
-    height: 420,
-    alignSelf: "center",
-    marginBottom: 20,
-  },
-  name: {
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  type: {
-    fontSize: 20,
-    color: "#666",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  hp: {
-    fontSize: 20,
-    color: "#666",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  subTitle: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  ability: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  attack: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  weakness: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  set: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-});
+const $detailContainerStyle: ViewStyle = {
+  marginTop: 16,
+};
+
+const $labelContainer: ViewStyle = {
+  gap: 8,
+  paddingVertical: 16,
+};
+const $contentContainerStyle: ViewStyle = {
+  padding: 8,
+};
+
+const $imageStyle: ImageStyle = {
+  width: 300,
+  height: 420,
+  alignSelf: "center",
+};
+
+const $labelStyle: TextStyle = {
+  fontSize: 18,
+  fontWeight: "bold",
+};
+
+const $buttonStyle: ViewStyle = {
+  padding: 8,
+  borderRadius: 10,
+  borderWidth: 1,
+  borderColor: "black",
+  alignSelf: "flex-start",
+};
+
+const $buttonContainerStyle: ViewStyle = {
+  flexWrap: "wrap",
+  flexDirection: "row",
+  gap: 8,
+  marginTop: 8,
+};
 
 export default PokemonScreen;
